700dfeae053bf8fcbd50e835c1141e7c9b8d3676
vladimirFr89 <silversssnake89@gmail.com>
2018-04-28 14:45:51 +0500

    Подключаем модуль vendorScript.js в файле app.js Добавил две функции для проверки работы с модулем vendorScript.js Добавлена зависимость exports-loader для использования В package для удобствa в скрипт dev добавлен параметр для показа source-map В конфиг добалены resolve для более удобного подключения внешних модулей а также exports-loader

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,23 @@
 'use strict';
 
 import angular from 'angular';
+//import vendorLib from '../vendors/vendorsLib/dist/vendorScript';
+/*
+* или, если в конфиге добавить resolve.alias, то можно написать удобнее
+*/
+//import vendorLib from 'vendorScript';
+
+/*
+* Должен быть установлен exports-loader, чтобы экспортировать из подключенного модуля
+ */
+//import vendorLib from 'exports-loader?getModuleInfo!vendorScript';
+/*
+* а также если в конфиге прописать лоадер, то импорт напишется проще
+ */
+import vendorLib from 'vendorScript';
 
 alert("Welcome to new angular app!");
 
 let app = angular.module('app', []);
+
+alert(vendorLib());
