19529bebf070f71726f14906aec33b914522fa04
vladimirFr89 <silversssnake89@gmail.com>
2018-04-28 09:12:31 +0500

    Используем Jquery в app.js Подключили jquery через CDN и дабавили немного html и css Поправили конфиг

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-alert('Welcome to webpack learning with using external libs!');
+//для подключения JQuery используем
+/*
+* Будет ошибка, если в конфиг не добавить externals, либо пока не установить jquery через npm*/
+let $ = require('jquery');
+
+$(function() {
+    console.log( "ready!" );
+    alert('Welcome to webpack learning with using external libs!');
+});
